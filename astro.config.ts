568297132f99c16475e57396c27c385848fd6ebe
sun <xinsun.cs@gmail.com>
2025-10-08 22:38:36 +0800

    修改为 cf 部署

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,7 @@ import AstroPureIntegration from 'astro-pure'
 import { defineConfig } from 'astro/config'
 import rehypeKatex from 'rehype-katex'
 import remarkMath from 'remark-math'
+import cloudflare from '@astrojs/cloudflare'
 
 // Others
 // import { visualizer } from 'rollup-plugin-visualizer'
@@ -33,7 +34,8 @@ export default defineConfig({
   // Adapter
   // https://docs.astro.build/en/guides/deploy/
   // 1. Vercel (serverless)
-  adapter: vercel(),
+  // adapter: vercel(),
+  adapter: cloudflare(),
   output: 'server',
   // 2. Vercel (static)
   // adapter: vercelStatic(),
